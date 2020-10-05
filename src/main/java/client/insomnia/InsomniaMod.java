616b9434e7d494f98aa343381dbc4d17554581f3
yagel15637 <yagelsharfi@gmail.com>
2020-10-05 17:52:56 +0300

    initial

---
+++ src/main/java/client/insomnia/InsomniaMod.java
@@ -0,0 +1,38 @@
+package client.insomnia;
+
+import client.insomnia.mixin.InsomniaMixinLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = InsomniaMod.MOD_ID,
+        name = InsomniaMod.MOD_NAME,
+        version = InsomniaMod.VERSION
+)
+public class InsomniaMod {
+    public static final String MOD_ID = "InsomniaMod";
+    public static final String MOD_NAME = "Insomnia";
+    public static final String VERSION = "0.1a";
+
+    public static InsomniaMixinLoader loader;
+
+    @Mod.Instance(MOD_ID)
+    public static InsomniaMod INSTANCE;
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        loader = new InsomniaMixinLoader();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/client/insomnia/mixin/mixins/MixinPlaceholder.java
@@ -0,0 +1,10 @@
+package client.insomnia.mixin.mixins;
+
+import client.insomnia.InsomniaMod;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(InsomniaMod.class)
+public class MixinPlaceholder {
+    // Here so project builds, if it won't be here when you import the project
+    // the directory client/insomnia/mixin/mixins won't exist and it'll break the build process.
+}
