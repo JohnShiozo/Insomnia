c87bb35515d8d185815fe0281c831a58ec3eb0f5
JohnShiozo <66097391+JohnShiozo@users.noreply.github.com>
2020-12-29 20:24:04 -0300

    Update InsomniaMod.java

--- src/main/java/client/insomnia/InsomniaMod.java
+++ src/main/java/client/insomnia/InsomniaMod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 public class InsomniaMod {
     public static final String MOD_ID = "InsomniaMod";
     public static final String MOD_NAME = "Insomnia";
-    public static final String VERSION = "0.1a";
+    public static final String VERSION = "0.1";
 
     public static InsomniaMixinLoader loader;
 
